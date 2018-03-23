4e7f9cd5e5b291666a273251095850ce9296f196
Leanderson André <leanderson.andre@gmail.com>
2018-03-23 17:08:14 -0300

    Create processo.c

---
+++ processo.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char **argv)
+{
+    char *name = argv[0];
+    int pid = fork();
+    if (pid == 0) {
+        printf("    ->Filho de %s tem PID %d\n", name, pid);
+    } else {
+        printf("Meu filho é %d\n", pid);
+        printf("Vou esperar o meu filho executar...\n");
+        wait(NULL);
+        printf("Filho terminou\n");
+    }
+    return 0; 
+}
